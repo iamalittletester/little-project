e44fca06790d4f156e4addecb3b26b79e6b7415a
iamalittletester <capreoara6@gmail.com>
2022-01-25 15:38:31 +0200

    Dependency updates.

--- src/main/java/com/imalittletester/browser/SetupHelper.java
+++ src/main/java/com/imalittletester/browser/SetupHelper.java
@@ -3,6 +3,7 @@ package com.imalittletester.browser;
 import org.openqa.selenium.WebDriver;
 
 public class SetupHelper {
+
     protected WebDriver driver;
     protected BrowserGetter browserGetter = new BrowserGetter();
 
