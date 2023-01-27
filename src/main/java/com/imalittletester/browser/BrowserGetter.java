719b0d7a40bc5fa0058d332e045a0fccad1efa75
iamalittletester <capreoara6@gmail.com>
2023-01-27 20:59:41 +0200

    Switch dependency from selenium-java to waiter2. Remove drivers. Update methods for initializing browsers.

--- src/main/java/com/imalittletester/browser/BrowserGetter.java
+++ src/main/java/com/imalittletester/browser/BrowserGetter.java
@@ -9,52 +9,13 @@ import static java.lang.System.setProperty;
 import static org.apache.commons.lang3.SystemUtils.*;
 
 public class BrowserGetter {
-    /**
-     * detect operating system
-     * if it is not one of the predefined ones, just throw exception
-     * only create a webDriver instance for a known operating system
-     */
-
-    public WebDriver getWinChromeDriver() {
-        setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        return driver;
-    }
-
     public WebDriver getChromeDriver() {
-        if (!IS_OS_WINDOWS && !IS_OS_LINUX && !IS_OS_MAC) {
-            throw new RuntimeException("Could not initialize browser due to unknown operating system!");
-        }
-        if (IS_OS_WINDOWS) {
-            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriver.exe");
-        }
-        if (IS_OS_LINUX) {
-            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriver");
-        }
-        if (IS_OS_MAC) {
-            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriverMac");
-        }
-
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         return driver;
     }
 
     public WebDriver getFirefoxDriver() {
-        if (!IS_OS_WINDOWS && !IS_OS_LINUX && !IS_OS_MAC) {
-            throw new RuntimeException("Could not initialize browser due to unknown operating system!");
-        }
-        if (IS_OS_WINDOWS) {
-            setProperty("webdriver.gecko.driver", "src/test/resources/browserBinaries/geckodriver.exe");
-        }
-        if (IS_OS_LINUX) {
-            setProperty("webdriver.gecko.driver", "src/test/resources/browserBinaries/geckodriver");
-        }
-        if (IS_OS_MAC) {
-            setProperty("webdriver.gecko.driver", "src/test/resources/browserBinaries/geckodriverMac");
-        }
-
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         return driver;
@@ -74,19 +35,6 @@ public class BrowserGetter {
     }
 
     public WebDriver getChromeDriverCustomSize(int width, int height) {
-        if (!IS_OS_WINDOWS && !IS_OS_LINUX && !IS_OS_MAC) {
-            throw new RuntimeException("Could not initialize browser due to unknown operating system!");
-        }
-        if (IS_OS_WINDOWS) {
-            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriver.exe");
-        }
-        if (IS_OS_LINUX) {
-            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriver");
-        }
-        if (IS_OS_MAC) {
-            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriverMac");
-        }
-
         WebDriver driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(width, height));
         return driver;
